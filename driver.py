cbea569a69e3e84f907c4accb49a8682282bae1c
looselycoupled <allen.leis@gmail.com>
2018-04-21 14:02:07 -0400

    adds skeleton app files

---
+++ driver.py
@@ -0,0 +1,41 @@
+# package.module
+# module description
+#
+# Author:   Allen Leis <allen.leis@gmail.com>
+# Created:  timestamp
+#
+# Copyright (C) 2017 Allen Leis
+# For license information, see LICENSE
+#
+# ID: filename.py [] allen.leis@gmail.com $
+
+"""
+module description
+"""
+
+##########################################################################
+# Imports
+##########################################################################
+
+import argparse
+
+
+##########################################################################
+# Classes
+##########################################################################
+
+
+##########################################################################
+# Execution
+##########################################################################
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Exercise DQN')
+    # parser.add_argument('integers', metavar='N', type=int, nargs='+',
+    #                     help='an integer for the accumulator')
+    # parser.add_argument('--sum', dest='accumulate', action='store_const',
+    #                     const=sum, default=max,
+    #                     help='sum the integers (default: find the max)')
+
+    args = parser.parse_args()
+    print(args)
